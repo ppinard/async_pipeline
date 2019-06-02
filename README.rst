593e143c077e34cda30295ae13ecefc0c1cfde05
Philippe Pinard <philippe.pinard@gmail.com>
2019-06-02 21:53:24 +0100

    Initial commit

---
+++ README.rst
@@ -0,0 +1,52 @@
+==============
+async pipeline
+==============
+
+.. image:: https://img.shields.io/pypi/v/async_pipeline.svg
+        :target: https://pypi.python.org/pypi/async_pipeline
+
+.. image:: https://img.shields.io/travis/ppinard/async_pipeline.svg
+        :target: https://travis-ci.org/ppinard/async_pipeline
+
+.. image:: https://readthedocs.org/projects/async-pipeline/badge/?version=latest
+        :target: https://async-pipeline.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+Execute pipeline asynchronously
+
+
+Installation
+============
+
+Easiest way to install using ``pip``::
+
+    pip install async_pipeline
+
+For development installation from the git repository::
+
+    git clone git@github.com/ppinard/async_pipeline.git
+    cd async_pipeline
+    pip install -e .
+
+Release notes
+=============
+
+0.1.0
+-----
+
+
+Contributors
+============
+
+
+License
+=======
+
+The library is provided under the Apache Software License 2.0 license.
+
+Copyright (c) 2019, Philippe Pinard
+
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+# Standard library modules.
+import os
+import sys
+import logging
+
+# Third party modules.
+from setuptools import setup, find_packages
+
+# Local modules.
+import versioneer
+
+# Globals and constants variables.
+logger = logging.getLogger(__name__)
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(BASEDIR, 'README.rst'), 'r') as fp:
+    LONG_DESCRIPTION = fp.read()
+
+PACKAGES = find_packages(include=['async_pipeline'])
+
+with open(os.path.join(BASEDIR, 'requirements.txt'), 'r') as fp:
+    INSTALL_REQUIRES = fp.read().splitlines()
+
+EXTRAS_REQUIRE = {}
+with open(os.path.join(BASEDIR, 'requirements_dev.txt'), 'r') as fp:
+    EXTRAS_REQUIRE['dev'] = fp.read().splitlines()
+
+CMDCLASS = versioneer.get_cmdclass()
+
+ENTRY_POINTS = {}
+
+setup(
+    name='async_pipeline',
+    version=versioneer.get_version(),
+    url='https://github.com/ppinard/async_pipeline',
+    author="Philippe Pinard",
+    author_email='philippe.pinard@gmail.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description="Execute pipeline asynchronously",
+    install_requires=requirements,
+    license="Apache Software License 2.0",
+    long_description=LONG_DESCRIPTION
+    include_package_data=True,
+
+    packages=PACKAGES,
+
+    install_requires=INSTALL_REQUIRES,
+    extras_require=EXTRAS_REQUIRE,
+
+    cmdclass=CMDCLASS,
+
+    entry_points=ENTRY_POINTS,
+)
+
